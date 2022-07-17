f2c0bbc25d0bbeb5ef3db766781e1c1af0994e28
cdspurlock <107364140+cdspurlock@users.noreply.github.com>
2022-07-16 21:40:58 -0500

    Add files via upload
    This is week one of ITDE class at Full Sail University. This is the CE01 project that i did and got an A on for all those that need the help on it.

---
+++ CE01-Get-Connected/JS/script.js
@@ -0,0 +1,7 @@
+// My Alert
+alert("My JavaScript file is connected properly");
+
+// This shows up in console for other editor to see.
+console.log("I can display text in the Console!");
+
+// practics
